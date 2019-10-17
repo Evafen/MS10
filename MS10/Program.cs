b0559678def8ecc2d9c68e53b2618b84d5683b75
Eva Fenyvesi <Eva.Fenyvesi@kantar.com>
2019-10-17 16:03:55 +0100

    Added spaces

--- MS10/Program.cs
+++ MS10/Program.cs
@@ -13,7 +13,7 @@ namespace MS10
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {  
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginPage());
